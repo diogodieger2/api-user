955075af5b241022b612399aef1b267fef387026
diogodieger4 <joaonogueira91893@gmail.com>
2025-08-31 11:14:13 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import express, { json } from "express"
-import userRoutes from "./routes/userRoutes"
+import express, { json } from "express";
+import userRoutes from "./routes/userRoutes";
+import cors from "cors";
+
 const app = express();
-const PORT = 3001
+const PORT = 3001;
 
-app.use(express.json())
-app.use('/users', userRoutes)
-
-
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-type", "Authorization"],
+  })
+);
 
 app.listen(PORT, () => {
-    console.log('APP INICIALIZADO COM SUCESSO', PORT)
-})
-
+  console.log("APP INICIALIZADO COM SUCESSO", PORT);
+});
