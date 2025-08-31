8459201b0d5817a1c92058d142cc57894247a17f
diogodieger4 <joaonogueira91893@gmail.com>
2025-08-31 11:34:00 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ app.use(
   cors({
     origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 
